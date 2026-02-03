c5d32d82d1c540f67f457b362a23531c48fac954
jdjmr15 <jmarquezcl1@gmail.com>
2026-02-02 21:30:50 -0300

    Git actions

---
+++ test_hello.py
@@ -0,0 +1,13 @@
+from hello import more_hello, more_goodbye
+
+
+def test_more_hello():
+    assert "hi" == more_hello()
+
+
+def test_more_hello_not_empt():
+    assert more_hello() != ""
+
+
+def test_more_goodbye():
+    assert "bye" == more_goodbye()
